ac1f1b53c8d2b18fa5ec6768525fecdc93cdaa30
sushi <2087152947@qq.com>
2024-08-21 13:38:26 +0800

    相机

--- index.js
+++ index.js
@@ -1,151 +1,138 @@
-class Ghost {
-  constructor(el) {
-    this.scene = el;
-    this.clone = el.cloneNode(true);
-    this.isEscaping = false;
-    this.ghost = el.querySelector('.ghost');
-    this.face = el.querySelector('.ghost-face');
-    this.eyes = el.querySelector('.eyes-row');
-    this.leftEye = this.eyes.querySelector('.left');
-    this.rightEye = this.eyes.querySelector('.right');
-    this.mouth = el.querySelector('.mouth');
-    this.mouthState = 'neutral';
-    this.shadow = el.querySelector('.shadow-container');
-    this.leftCheek = el.querySelector('.left.cheek');
-    this.leftCheek = el.querySelector('.right.cheek');
-    this.leftHand = el.querySelector('.hand-left');
-    this.rightHand = el.querySelector('.right-hand');
-    this.activityInterval = null;
-  }
+var rotateDiv = document.getElementById('rot');
+var rotateIcons = document.getElementById('rot-icons');
+var clickRotateDiv = document.getElementById('click-rot');
+var angle = 0;
 
-  reset() {
-    this.scene.remove();
-    this.scene = this.clone.cloneNode(true);
-    this.resetRefs();
-    this.scene.classList.remove('stars-out');
-    this.scene.style.position = 'absolute';
-    this.scene.style.left = Math.floor(Math.random() * (document.querySelector('body').getBoundingClientRect().width - 140)) + 'px';
-    this.scene.style.top = Math.floor(Math.random() * (document.querySelector('body').getBoundingClientRect().height - 160)) + 'px';
-    this.scene.classList.add('descend');
-    this.shadow.classList.add('disappear');
-    document.querySelector('body').append(this.scene);
-    this.enter();
-  }
+clickRotateDiv.onclick = function () {
+  angle += 60;
+  rotateDiv.style.transform = 'rotate(' + angle + 'deg)';
+  rotateIcons.style.transform = 'rotate(' + angle + 'deg)';
+};
 
-  resetRefs() {
-    this.ghost = this.scene.querySelector('.ghost');
-    this.face = this.scene.querySelector('.ghost-face');
-    this.eyes = this.scene.querySelector('.eyes-row');
-    this.leftEye = this.eyes.querySelector('.left');
-    this.rightEye = this.eyes.querySelector('.right');
-    this.mouth = this.scene.querySelector('.mouth');
-    this.mouthState = 'neutral';
-    this.isEscaping = false;
-    this.shadow = this.scene.querySelector('.shadow-container');
-    this.leftCheek = this.scene.querySelector('.left.cheek');
-    this.leftCheek = this.scene.querySelector('.right.cheek');
-    this.leftHand = this.scene.querySelector('.hand-left');
-    this.rightHand = this.scene.querySelector('.right-hand');
-  }
+var step = 2;
+var color1 = 'rgba(0,0,0,0.5)';
+var color2 = 'rgba(0,0,0,0.1)';
 
-  blink() {
-    this.leftEye.classList.add('blink');
-    this.rightEye.classList.add('blink');
-    setTimeout(() => {
-      this.leftEye.classList.remove('blink');
-      this.rightEye.classList.remove('blink');
-    }, 50)
-  }
+var gradient = ' conic-gradient(';
+for (var i = 0; i < 360; i += step) {
+  var color = i % (2 * step) === 0 ? color1 : color2;
+  gradient += color + ' ' + i + 'deg, ';
+}
+gradient = gradient.slice(0, -2) + '), rgb(85 93 108)';
 
-  wave() {
-    this.leftHand.classList.add('waving');
-    setTimeout(() => {
-      this.leftHand.classList.remove('waving');
-    }, 500);
-  }
+rotateDiv.style.background = gradient;
 
-  openMouth() {
-    this.mouth.classList.remove('closed');
-    this.mouth.classList.add('open');
-    this.mouthState = 'open';
-  }
 
-  closeMouth() {
-    this.mouth.classList.remove('open');
-    this.mouth.classList.add('closed');
-    this.mouthState = 'closed';
-  }
+var toggles = document.querySelectorAll('.toggle');
+var tempElement = document.querySelector('.temp');
 
-  neutralMouth() {
-    this.mouth.classList.remove('open');
-    this.mouth.classList.remove('closed');
-    this.mouthState = 'neutral';
-  }
+let isAnimating = false; // Add flag to indicate if animation is active
 
-  hover() {
-    this.ghost.classList.add('hover');
-  }
+toggles.forEach(function (toggle) {
+  toggle.addEventListener('click', function () {
+    if (this.classList.contains('active') || isAnimating) { // Check if animation is active
+      return;
+    }
+    toggles.forEach(function (toggle) {
+      toggle.classList.remove('active');
+    });
+    this.classList.add('active');
+    var tempValue = parseFloat(tempElement.textContent);
+    if (this.id === 'toggle-cel') {
+      var celsius = Math.round((tempValue - 32) * 5 / 9);
+      tempElement.textContent = celsius + '°C';
+    } else if (this.id === 'toggle-far') {
+      var fahrenheit = Math.round(tempValue * 9 / 5 + 32);
+      tempElement.textContent = fahrenheit + '°F';
+    }
+  });
+});
 
-  surprise() {
-    this.face.classList.add('surprised');
-  }
+let currentTempF = 34; // Initialize with the initial temperature in Fahrenheit
 
-  runAway() {
-    clearInterval(this.activityInterval);
-    if (!this.isEscaping) {
-      this.isEscaping = true;
-      this.scene.classList.add('run-away', 'move-stars-in');
-      this.scene.classList.remove('stars-out');
-      setTimeout(() => {
-        this.shadow.classList.add('disappear');
-        setTimeout(() => {
-          this.reset();
-        }, Math.floor(Math.random() * 1000) + 500);
-      }, 450);
+// cubic ease in/out function
+function easeInOutCubic(t) {
+  return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+}
+
+function changeTemp(element, newTemp) {
+  let unit = element.innerHTML.includes("F") ? "°F" : "°C";
+  let currentTemp = unit === "°F" ? currentTempF : Math.round((currentTempF - 32) * 5 / 9);
+  let finalTemp = unit === "°F" ? newTemp : Math.round((newTemp - 32) * 5 / 9);
+
+  let duration = 2000; // Duration of the animation in milliseconds
+  let startTime = null;
+
+  function animate(currentTime) {
+    if (startTime === null) {
+      startTime = currentTime;
+    }
+
+    let elapsed = currentTime - startTime;
+    let progress = Math.min(elapsed / duration, 1);
+    progress = easeInOutCubic(progress);
+
+    let tempNow = Math.round(currentTemp + (progress * (finalTemp - currentTemp)));
+    element.innerHTML = `${tempNow}${unit}`;
+
+    if (progress < 1) {
+      requestAnimationFrame(animate);
+    } else {
+      // Update currentTempF once the animation is complete
+      currentTempF = newTemp;
+      isAnimating = false; // Reset the flag when animation is done
     }
   }
 
-  enter() {
-    setTimeout(() => {
-      this.shadow.classList.remove('disappear');
-    }, 5);
-    setTimeout(() => {
-      this.scene.classList.remove('descend');
-      this.scene.classList.add('stars-out', 'move-stars-out');
-    }, 600);
-    setTimeout(() => {
-      this.hover();
-      this.prepareEscape();
-      this.startActivity();
-    }, 1200)
-  }
-
-  startActivity() {
-    this.activityInterval = setInterval(() => {
-      switch (Math.floor(Math.random() * 4)) {
-        case 0:
-          this.blink();
-          setTimeout(() => { this.blink() }, 400);
-          setTimeout(() => { this.blink() }, 1300);
-          break;
-        case 1:
-          this.wave();
-          break;
-        default:
-          break;
-      }
-    }, 7000);
-  }
-
-  prepareEscape() {
-    this.scene.addEventListener('click', () => { this.runAway() }, false);
-    this.scene.addEventListener('mouseover', () => { this.runAway() }, false);
-    this.scene.addEventListener('focus', () => { this.runAway() }, false);
-  }
+  isAnimating = true; // Set flag when animation starts
+  requestAnimationFrame(animate);
 }
 
-let ghost = new Ghost(document.querySelector('.scene-container'));
 
-ghost.hover();
-ghost.startActivity();
-ghost.prepareEscape();
+window.onload = function () {
+  const sixths = Array.from(document.querySelectorAll('.sixths'));
+  let index = 0;
+  let temp = document.querySelector('.temp');
+
+  document.querySelector('#rot-icons').addEventListener('click', () => {
+    sixths[index].classList.remove('active');
+    index = (index + 1) % sixths.length;
+    sixths[index].classList.add('active');
+    if (index == 0) {
+      changeTemp(temp, 34);
+      console.log("sun")
+      document.querySelector('#mountains').classList.remove("snow");
+      document.querySelector('#mountains').classList.remove("clouds");
+    } else if (index == 1) {
+      changeTemp(temp, 27);
+      console.log("sunset")
+      document.querySelector('#mountains').classList.add("sunset");
+    } else if (index == 2) {
+      changeTemp(temp, 14);
+      console.log("moon")
+      document.querySelector('#mountains').classList.remove("sunset");
+      document.querySelector('#mountains').classList.add("moon");
+    } else if (index == 3) {
+      changeTemp(temp, 16);
+      console.log("clouds")
+      document.querySelector('#mountains').classList.add("clouds");
+    } else if (index == 4) {
+      changeTemp(temp, 8);
+      console.log("storm")
+      document.querySelector('#mountains').classList.add("storm");
+    } else if (index == 5) {
+      changeTemp(temp, -4);
+      console.log("snow")
+      document.querySelector('#mountains').classList.remove("moon");
+      document.querySelector('#mountains').classList.remove("storm");
+      document.querySelector('#mountains').classList.add("snow");
+    }
+
+    let loadingBar = document.querySelector('.loading-bar');
+    loadingBar.classList.add('active');
+
+    setTimeout(() => {
+      loadingBar.classList.remove('active');
+    }, 1200);
+  });
+};
